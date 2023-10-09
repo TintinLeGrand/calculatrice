2f41ea887df4c782161665cfc042fb00578b1c82
Ethan <ethanleneindre@etu.unicaen.fr>
2023-10-09 17:57:55 +0200

    init: Init files and icon #1

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/svg" href="calculatrice.svg" />
+
+    <title>Calculatrice - Ethan</title>
+</head>
+<body>
+    <?php
+        echo 'test';
+    ?>
+</body>
+</html>
